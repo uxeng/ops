b4f4cd0a1585e8add94dec7aeea7f2fdef279873
Victor Bayon <victor@innowatts.com>
2025-12-05 17:57:12 +0000

    Updated

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -5,6 +5,7 @@ This package provides basic functionality for testing and development.
 
 __version__ = "4.0.0"
 
+
 def main():
     """Entry point"""
-    print('Starting')
+    print("Starting")
--- tests/sample/test_app.py
+++ tests/sample/test_app.py
@@ -13,5 +13,5 @@ class TestSimple(unittest.TestCase):
         self.assertEqual(add_one(5), 6)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

507a5e93221ff59b91b16f9b7b9f9b0065d14eca
Victor Bayon <victor@innowatts.com>
2026-01-19 08:39:05 +0000

    Updated

--- src/sample/app.py
+++ src/sample/app.py
@@ -1,13 +1,12 @@
 """Sample web application"""
 
-import os
 import logging
+import os
 from random import random
 
+from flask import Flask
 from pythonjsonlogger import jsonlogger
 
-import logging
-from pythonjsonlogger import jsonlogger
 logHandler = logging.StreamHandler()
 formatter = jsonlogger.JsonFormatter()
 logHandler.setFormatter(formatter)
@@ -16,11 +15,6 @@ logger.addHandler(logHandler)
 
 log_level = os.environ.get("LOG_LEVEL", logging.INFO)
 logger.setLevel(log_level)
-
-
-
-from flask import Flask
-
 
 
 def add_one(number: int) -> int:
@@ -38,22 +32,29 @@ def add_one(number: int) -> int:
     """
     return number + 1
 
+
 app = Flask(__name__)
 
 
 def get_tcp_port() -> int:
-    """Gets the tcp port configuration from the environment. 
-    If the env var PYTHON_APP_TCP_PORT is not present the tcp port is set to the default value of 8080.
+    """Gets the tcp port configuration from the environment.
+    If the env var PYTHON_APP_TCP_PORT is not present the tcp port is set
+    to the default value of 8080.
 
     Returns:
-        the constructed string"""
+        The TCP port number as an integer.
+    """
     tcp_port: int = 8080
-    logger.info('Checking the following environment variable: %s', "PYTHON_APP_TCP_PORT")
+    logger.info(
+        "Checking the following environment variable: %s", "PYTHON_APP_TCP_PORT"
+    )
     try:
         tcp_port: int = int(os.environ.get("PYTHON_APP_TCP_PORT", tcp_port))
     except ValueError:
-        logger.error("Could not parse PYTHON_APP_TCP_PORT env variable %s", exc_info=True)
-    logger.info('Using the following TCP_PORT: %s', tcp_port)
+        logger.error(
+            "Could not parse PYTHON_APP_TCP_PORT env variable %s", exc_info=True
+        )
+    logger.info("Using the following TCP_PORT: %s", tcp_port)
     return tcp_port
 
 
@@ -71,15 +72,17 @@ def hello_worlds():
 
     message: str = "abcdefg"
 
-    logger.info(message, extra={
-    "application": "sample.app",
-    "utility": "utility-12345",
-    "className": __name__,
-    "url":"/",
-    "userid" : "user-123445",
-    "channel" : "http",
-    })
-    
+    logger.info(
+        message,
+        extra={
+            "application": "sample.app",
+            "utility": "utility-12345",
+            "className": __name__,
+            "url": "/",
+            "userid": "user-123445",
+            "channel": "http",
+        },
+    )
 
     return "<p>Hello " + str(add_one(int(1000 * random()))) + " Worlds!</p>"
 
@@ -87,99 +90,3 @@ def hello_worlds():
 if __name__ == "__main__":
     logger.info("Application starting")
     app.run(host="0.0.0.0", port=get_tcp_port())
-
-
-
-
-
-# structlog
-# """Application module providing basic mathematical operations."""
-
-# import os
-# import logging
-# from random import random
-
-# import structlog
-
-# from flask import Flask
-
-
-# def set_some_info(_, __, event_dict):
-#     """Process structlog event dictionary to add some info.
-
-#     Args:
-#         _: First argument (unused, required by structlog processor signature)
-#         __: Second argument (unused, required by structlog processor signature)
-#         event_dict: Dictionary containing log event data
-
-#     Returns:
-#         Modified event dictionary with 'set_some_info' key added
-#     """
-#     event_dict["set_some_info"] = "some-info"
-#     return event_dict
-
-
-# log_level = os.environ.get("LOG_LEVEL", logging.INFO)
-
-
-# structlog.configure(
-#     processors=[
-#         structlog.stdlib.add_log_level,
-#         structlog.processors.TimeStamper(fmt="iso"),
-#         set_some_info,
-#         structlog.processors.JSONRenderer(),
-#     ],
-#     wrapper_class=structlog.make_filtering_bound_logger(log_level),
-# )
-
-
-# logger = structlog.get_logger()
-
-
-# def add_one(number: int) -> int:
-#     """Add one to the given number.
-
-#     Args:
-#         number: The integer to increment.
-
-#     Returns:
-#         The input number incremented by one.
-
-#     Example:
-#         >>> add_one(5)
-#         6
-#     """
-#     return number + 1
-
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def hello_worlds():
-#     """Constructs an html compatible string based on a friendly message and a random number
-
-#     Returns:
-#         the constructed string"""
-
-#     # Log format reference:
-#     # %date{YYYY-MM-dd HH:mm:ss.SSS} %X{application} %X{url} %X{uuid}
-#     # %X{utility} thread="%thread" level="%-5level" className="%logger{36}.%M:%line"
-#     # %X{userId} %X{logChannel} %msg%n
-
-#     message: str = "abcdefg"
-#     logger.info(
-#         message,
-#         application="sample.app",
-#         utility="utility-12345",
-#         className=__name__,
-#         url="/",
-#         userid="user-123445",
-#         channel="http",
-#     )
-
-#     return "<p>Hello " + str(add_one(int(1000 * random()))) + " Worlds!</p>"
-
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=3000)
--- tests/sample/test_app.py
+++ tests/sample/test_app.py
@@ -1,4 +1,4 @@
-"""Unit tests for the simple module."""
+"""Unit tests for the sample.app module."""
 
 import unittest
 
@@ -7,22 +7,26 @@ from sample.app import get_tcp_port
 from sample.app import hello_worlds
 
 
-class TestSimpleApp(unittest.TestCase):
-    """Test cases for the methods in simple.app module."""
+class TestSampleApp(unittest.TestCase):
+    """Test cases for the methods in sample.app module."""
 
-    def test_get_tcp_port(self) -> None:
-        """Test that the get_tcp_port method returns an int."""
-        self.assertEqual(type(get_tcp_port(5)), int)
-
-    def test_generate_one(self) -> None:
+    def test_add_one(self) -> None:
         """Test that the add_one method correctly increments a number by one."""
         self.assertEqual(add_one(5), 6)
 
+    def test_get_tcp_port(self) -> None:
+        """Test that the get_tcp_port method returns an int."""
+        self.assertEqual(type(get_tcp_port()), int)
+
     def test_hello_worlds(self) -> None:
-        """Test that hello_worlds returns a string and that the string is formated as an html paragraph."""
+        """Test that hello_worlds returns a string formatted as an html paragraph."""
         hello_worlds_result: str = hello_worlds()
         self.assertEqual(type(hello_worlds_result), str)
-        self.assertTrue(hello_worlds_result.startswith("<p>") and hello_worlds_result.endswith("</p>"))
+        self.assertTrue(
+            hello_worlds_result.startswith("<p>")
+            and hello_worlds_result.endswith("</p>")
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
